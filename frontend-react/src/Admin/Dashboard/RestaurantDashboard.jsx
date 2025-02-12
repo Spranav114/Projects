6efba8732231dabe4c8a43980fbb5ac65b064489
unknown <pranavsugre114@gmail.com>
2025-02-12 23:40:12 +0530

    Added Project

---
+++ frontend-react/src/Admin/Dashboard/RestaurantDashboard.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getMenuItemsByRestaurantId } from "../../State/Customers/Menu/menu.action";
+import { Grid } from "@mui/material";
+import OrdersTable from "../Orders/OrderTable";
+import MenuItemTable from "../Food/MenuItemTable";
+import AvgCard from "../ReportCard/ReportCard";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+import SellIcon from "@mui/icons-material/Sell";
+import FastfoodIcon from "@mui/icons-material/Fastfood";
+
+const RestaurantDashboard = () => {
+  const { id } = useParams();
+  const {restaurant}=useSelector(store=>store);
+  console.log("restaurants id ", id);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      getMenuItemsByRestaurantId({
+        restaurantId: id,
+        jwt: localStorage.getItem("jwt"),
+      })
+    );
+  }, []);
+
+  console.log("restaurant",restaurant)
+  return (
+    <div className="px-2">
+      
+      <Grid container spacing={1}>
+     
+        <Grid lg={6} xs={12} item>
+          <OrdersTable name={"Recent Order"} isDashboard={true} />
+        </Grid>
+        <Grid lg={6} xs={12} item>
+          <MenuItemTable isDashboard={true} name={"Recently Added Menu"} />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default RestaurantDashboard;
