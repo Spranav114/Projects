71ac737de05de2f9966bd13149801c2b30f5b44a
Pranav Sugre <84894144+Spranav114@users.noreply.github.com>
2025-02-13 19:07:09 +0530

    Update store.js

--- frontend-react/src/State/Store/store.js
+++ frontend-react/src/State/Store/store.js
@@ -6,7 +6,7 @@ import menuItemReducer from "../Customers/Menu/Reducer";
 import cartReducer from "../Customers/Cart/Reducer";
 import { orderReducer } from "../Customers/Orders/order.reducer";
 import restaurantsOrderReducer from "../Admin/Order/restaurants.order.reducer";
-import superAdminReducer from "../SuperAdmin/superAdmin.reducer";
+
 import { ingredientReducer } from "../Admin/Ingredients/Reducer";
 
 
@@ -23,8 +23,7 @@ const rootReducer=combineReducers({
     restaurantsOrder:restaurantsOrderReducer,
     ingredients:ingredientReducer,
 
-    // super admin
-    superAdmin:superAdminReducer
+    
 })
 
 export const store=legacy_createStore(rootReducer,applyMiddleware(thunk))
